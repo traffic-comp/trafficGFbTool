79fd06131b2c0d883a5e08a7c3681967701b76e6
1alusha1 <alusha9636@gmail.com>
2025-07-16 12:18:35 +0300

    updated files page

--- src/app/main/tt/uploaded/page.jsx
+++ src/app/main/tt/uploaded/page.jsx
@@ -8,9 +8,7 @@ import useTTStore from "@/store/useTTStore";
 import { useState } from "react";
 
 const Uploaded = () => {
-  const { setLeads, leads } = useFBStore();
-  const { files, deleteFile } = useTTStore();
-  const { addMessage } = useErrorStore();
+  const { leads } = useFBStore();
 
   const [currentPage, setCurrentPage] = useState(1);
   const leadsPerPage = 12;
@@ -27,56 +25,8 @@ const Uploaded = () => {
     }
   };
 
-  const sendFileToServer = async (e) => {
-    if (!files.length) {
-      return addMessage("warning", "Нет файлов для загрузки!");
-    }
-    const formData = new FormData();
-
-    files.forEach(({ file }) => {
-      formData.append("files", file); // добавляем сами файлы
-    });
-
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URI}/tt/uploadLeadsfile`,
-      {
-        method: "POST",
-        body: formData,
-      }
-    );
-
-    const { data, msg } = await res.json();
-
-    addMessage("success", "Данные успешно загружены");
-    return setLeads(data);
-  };
   return (
     <>
-      <button
-        onClick={sendFileToServer}
-        className="px-[54px] py-[8px] text-center text-[18px] font-semibold border-2 border-[var(--color-main-blue)] rounded-[12px] bg-white cursor-pointer transition duration-300 uppercase mb-[20px]  hover:text-[var(--color-white)] hover:bg-[var(--color-main-blue)]"
-      >
-        Загрузить Лиды
-      </button>
-
-      <FormList
-        data={files}
-        click={() => deleteFile(file.id)}
-        renderRow={(file) => (
-          <>
-            <div className="flex w-full justify-between items-center gap-[10px]">
-              <span>{file.file.name}</span>
-              <span
-                className="bg-[#da4848] text-[18px] py-[9px] px-[38px] rounded-[12px] text-center text-[var(--color-white)]"
-                onClick={() => deleteFile(file.id)}
-              >
-                Удалить
-              </span>
-            </div>
-          </>
-        )}
-      />
-
       <RawLeadsSection
         currentLeads={currentLeads}
         goToPage={goToPage}
