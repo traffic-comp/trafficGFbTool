1f9143afd24e6e4549f2e22ab8eceb224c981ffa
1alusha1 <alusha9636@gmail.com>
2025-07-17 17:06:29 +0300

    fixed visual bug

--- src/app/components/FanpagesList.jsx
+++ src/app/components/FanpagesList.jsx
@@ -1,20 +1,20 @@
-'use client'
-import useFBStore from '@/store/useFbStore';
-import FanpageItem from '@/app/components/ui/FanpageItem';
+"use client";
+import useFBStore from "@/store/useFbStore";
+import FanpageItem from "@/app/components/ui/FanpageItem";
 
-const FanpagesList = ({ onItemClick}) => {
-  const { pages, } = useFBStore();
+const FanpagesList = ({ onItemClick }) => {
+  const { pages } = useFBStore();
 
   return (
     <div className="h-[100%] flex flex-col">
-      {pages.map((page) => (
-        <div className="grid grid-cols-3 gap-[30px]" key={page.id}>
+      <div className="grid grid-cols-3 gap-[30px]" key={page.id}>
+        {pages.map((page) => (
           <FanpageItem
             text={`${page.category} ${page.name}`}
             click={() => onItemClick(page.id, page.access_token)}
           />
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
