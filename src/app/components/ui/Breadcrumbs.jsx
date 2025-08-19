77caee4708211c2b6b1af7396543e5324d57a3b5
1alusha1 <alusha9636@gmail.com>
2025-08-19 12:07:16 +0300

    fixed bug with redirect to 404 page in breadcrumbs

--- src/app/components/ui/Breadcrumbs.jsx
+++ src/app/components/ui/Breadcrumbs.jsx
@@ -4,22 +4,25 @@ import { getBreadcrumbs } from "@/utils/getBreadcrumbs";
 
 const Breadcrumbs = () => {
   const router = useRouter();
-  const pathname = usePathname(); // например: "/main/fb/form"
+  const pathname = usePathname(); // например: "/fb/form"
   const crumbs = getBreadcrumbs(pathname); // ["fb", "form"]
 
   const handleClick = (index) => {
     const slicedPath = crumbs.slice(0, index + 1).join("/");
-    router.push(`/main/${slicedPath}`);
+
+    if (slicedPath === "tt") {
+      return router.push(`/${slicedPath}/files`);
+    } else if (slicedPath === "fb") {
+      return router.push(`/${slicedPath}/fanpages`);
+    }
+
+    router.push(`/${slicedPath}`);
   };
 
   return (
     <div className="font-bold">
       {crumbs.map((crumb, i) => (
-        <span
-          key={i}
-          onClick={() => handleClick(i)}
-          className="cursor-pointer"
-        >
+        <span key={i} onClick={() => handleClick(i)} className="cursor-pointer">
           {i !== 0 && <span> / </span>}
           {crumb}
         </span>
