2911659ded1290cc5cb0b1cee30730b526cc7592
Ilya Zahorodnii <70091202+1Alusha1@users.noreply.github.com>
2025-10-06 12:06:46 +0300

    Updated scopes for fb

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   const appId = process.env.NEXT_PUBLIC_APP_ID;
   const redirectUri = `${process.env.NEXT_PUBLIC_SITE_LINK}/callback`;
-  const scope = "pages_show_list, leads_retrieval, public_profile, email, pages_manage_ads, ads_read, ads_management";
+  const scope = "email, catalog_management, pages_show_list, ads_management, ads_read, business_management, leads_retrieval, attribution_read, pages_read_engagement, pages_manage_metadata, pages_manage_ads, public_profile";
   const responseType = "token";
 
   const fbAuthUrl = `https://www.facebook.com/v19.0/dialog/oauth?client_id=${appId}&redirect_uri=${encodeURIComponent(
